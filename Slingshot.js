28e34dcb4cab1d483353f03223e8e95cba0455fa
jaskaran <rashankaur867@gmail.com>
2020-08-16 17:07:00 +0530

    catpelt and rubber band

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,9 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.image1 = loadImage("sprites/sling1.png")
+        this.image2 = loadImage("sprites/sling2.png")
+        this.image3 = loadImage("sprites/sling3.png")
         World.add(world, this.sling);
     }
 
@@ -16,11 +19,30 @@ class SlingShot{
     }
 
     display(){
+
+        image(this.image1,200,75,40,150)
+        
+        image(this.image2,173,65,40,100)
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            push();
+            strokeWeight(8);
+            stroke("#3E1D0B");
+            if(pointA.x < 210){
+            line(pointA.x-20,pointA.y,pointB.x-10,pointB.y-3)
+            line(pointA.x-20,pointA.y,pointB.x+30,pointB.y+5 )
+            image(this.image3,pointA.x-25,pointA.y-10,10,30)
+        }else{
+            line(pointA.x+20,pointA.y,pointB.x-10,pointB.y-3)
+            line(pointA.x+20,pointA.y,pointB.x+30,pointB.y+5 )
+            image(this.image3,pointA.x+25,pointA.y-10,10,30)
+        }
+            pop();
+            
+
+           // line(pointA.x, pointA.y, pointB.x, pointB.y);
         }
     }
     
